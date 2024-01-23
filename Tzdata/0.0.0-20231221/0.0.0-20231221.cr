b2bb67d4a9af0af58a3dfaafa146e2c463cc6405
Fulgurance <zohran.londais@gmail.com>
2024-01-23 18:15:39 +0000

    Added tzdata

---
+++ Tzdata/0.0.0-20231221/0.0.0-20231221.cr
@@ -0,0 +1,32 @@
+class Target < ISM::Software
+    
+    def prepareInstallation
+        super
+
+        makeDirectory("#{builtSoftwareDirectoryPath(false)}/#{Ism.settings.rootPath}/usr/share/zoneinfo")
+        makeDirectory("#{builtSoftwareDirectoryPath(false)}/#{Ism.settings.rootPath}/usr/share/zoneinfo/posix")
+        makeDirectory("#{builtSoftwareDirectoryPath(false)}/#{Ism.settings.rootPath}/usr/share/zoneinfo/right")
+
+        [   "etcetera",
+            "southamerica",
+            "northamerica",
+            "europe",
+            "africa",
+            "antarctica",
+            "asia",
+            "australasia",
+            "backward"].each do |timezone|
+
+            runZicCommand(["zic","-L","/dev/null","-d","/usr/share/zoneinfo/",timezone], mainWorkDirectoryPath)
+            runZicCommand(["zic","-L","/dev/null","-d","/usr/share/zoneinfo/posix",timezone], mainWorkDirectoryPath)
+            runZicCommand(["zic","-L","leapseconds","-d","/usr/share/zoneinfo/right",timezone], mainWorkDirectoryPath)
+        end
+
+        copyFile("#{mainWorkDirectoryPath(false)}/zone.tab","#{builtSoftwareDirectoryPath(false)}/#{Ism.settings.rootPath}/usr/share/zoneinfo/zone.tab")
+        copyFile("#{mainWorkDirectoryPath(false)}/zone1970.tab","#{builtSoftwareDirectoryPath(false)}/#{Ism.settings.rootPath}/usr/share/zoneinfo/zone1970.tab")
+        copyFile("#{mainWorkDirectoryPath(false)}/iso3166.tab","#{builtSoftwareDirectoryPath(false)}/#{Ism.settings.rootPath}/usr/share/zoneinfo/iso3166.tab")
+
+        runZicCommand(["zic","-d","/usr/share/zoneinfo","-p","America/New_York"], mainWorkDirectoryPath)
+    end
+
+end
