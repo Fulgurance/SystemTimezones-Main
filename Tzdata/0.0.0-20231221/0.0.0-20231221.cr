8feb0a4dd90808e8b7781481daa27cbd73c321a6
Fulgurance <zohran.londais@gmail.com>
2024-06-10 12:36:36 +0100

    Updated to new ism standard (much easiest implementation)

--- Tzdata/0.0.0-20231221/0.0.0-20231221.cr
+++ Tzdata/0.0.0-20231221/0.0.0-20231221.cr
@@ -3,9 +3,9 @@ class Target < ISM::Software
     def prepareInstallation
         super
 
-        makeDirectory("#{builtSoftwareDirectoryPath(false)}/#{Ism.settings.rootPath}/usr/share/zoneinfo")
-        makeDirectory("#{builtSoftwareDirectoryPath(false)}/#{Ism.settings.rootPath}/usr/share/zoneinfo/posix")
-        makeDirectory("#{builtSoftwareDirectoryPath(false)}/#{Ism.settings.rootPath}/usr/share/zoneinfo/right")
+        makeDirectory("#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}/usr/share/zoneinfo")
+        makeDirectory("#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}/usr/share/zoneinfo/posix")
+        makeDirectory("#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}/usr/share/zoneinfo/right")
 
         [   "etcetera",
             "southamerica",
@@ -22,9 +22,9 @@ class Target < ISM::Software
             runZicCommand(["-L","leapseconds","-d","/usr/share/zoneinfo/right",timezone], mainWorkDirectoryPath)
         end
 
-        copyFile("#{mainWorkDirectoryPath(false)}/zone.tab","#{builtSoftwareDirectoryPath(false)}/#{Ism.settings.rootPath}/usr/share/zoneinfo/zone.tab")
-        copyFile("#{mainWorkDirectoryPath(false)}/zone1970.tab","#{builtSoftwareDirectoryPath(false)}/#{Ism.settings.rootPath}/usr/share/zoneinfo/zone1970.tab")
-        copyFile("#{mainWorkDirectoryPath(false)}/iso3166.tab","#{builtSoftwareDirectoryPath(false)}/#{Ism.settings.rootPath}/usr/share/zoneinfo/iso3166.tab")
+        copyFile("#{mainWorkDirectoryPath}/zone.tab","#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}/usr/share/zoneinfo/zone.tab")
+        copyFile("#{mainWorkDirectoryPath}/zone1970.tab","#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}/usr/share/zoneinfo/zone1970.tab")
+        copyFile("#{mainWorkDirectoryPath}/iso3166.tab","#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}/usr/share/zoneinfo/iso3166.tab")
 
         runZicCommand(["-d","/usr/share/zoneinfo","-p","America/New_York"], mainWorkDirectoryPath)
     end
