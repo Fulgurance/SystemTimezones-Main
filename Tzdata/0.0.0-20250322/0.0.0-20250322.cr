d68b30f93de595af6f4098bf6e426d824d86b5b1
Fulgurance <zohran.londais@gmail.com>
2025-07-21 15:01:26 +0100

    Necessary patch

--- Tzdata/0.0.0-20250322/0.0.0-20250322.cr
+++ Tzdata/0.0.0-20250322/0.0.0-20250322.cr
@@ -3,7 +3,8 @@ class Target < ISM::Software
     def build
         super
 
-        makeSource(path: buildDirectoryPath)
+        makeSource( path: buildDirectoryPath,
+                    environment: {"CC" => "gcc"})
     end
 
     def prepareInstallation
